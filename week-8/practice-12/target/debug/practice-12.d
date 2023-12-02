2b647d4e267f248b17f299412b97ad2f49c7d2ec
v.falowocos101 <victoria.falowo@pau.edu.ng>
2023-12-02 22:06:19 +0100

    (Week 8 commit)

---
+++ week-8/practice-12/target/debug/practice-12.d
@@ -0,0 +1,1 @@
+C:\Users\veefa\OneDrive\Documents\v.falowoCOS101\week-8\practice-12\target\debug\practice-12.exe: C:\Users\veefa\OneDrive\Documents\v.falowoCOS101\week-8\practice-12\src\main.rs
---
+++ week-8/practice-3/target/debug/practice-3.d
@@ -0,0 +1,1 @@
+C:\Users\veefa\OneDrive\Documents\v.falowoCOS101\week-8\practice-3\target\debug\practice-3.exe: C:\Users\veefa\OneDrive\Documents\v.falowoCOS101\week-8\practice-3\src\main.rs
